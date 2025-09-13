8c6b9ab4fe9c33132f9524fc70720f0ce004435a
Manivannan <yuvanmani007@gmail.com>
2025-09-13 12:13:16 +0530

    react router setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
+import { createBrowserRouter, RouterProvider } from "react-router"
+
+const routes = [{
+  path: "/",
+  element: <h1 className="text-red-700 font-bold m-5 text-3xl">Quote Generator</h1>
+}]
+
+const router = createBrowserRouter(routes, {
+  future: {
+    v7_relativeSplatPath: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
+  }
+})
+
 const App = () => {
   return (
-    <h1 className="text-red-700 font-bold m-5 text-3xl">Quote Generator</h1>
+    <RouterProvider
+      router={router}
+      future={{
+        v7_startTransition: true,
+      }}
+    />
   )
 }
 
